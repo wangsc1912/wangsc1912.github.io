26ed893a389a0e2ab69dbda26590daad1bf026e0
Shaocong <wangshaocong@wangshaocongdeMacBook-Pro.local>
2026-08-27 19:43:12 +0800

    update.

--- src/components/Home/IntroSection.tsx
+++ src/components/Home/IntroSection.tsx
@@ -3,6 +3,7 @@ import React from "react";
 const researchThemes = [
     "Memory-Centric AI Systems",
     "Computing-in-Memory Technologies",
+    "(and more generalized) Efficient AI",
 ];
 
 const IntroSection: React.FC = () => (
--- src/components/Home/RecruitmentSection.tsx
+++ src/components/Home/RecruitmentSection.tsx
@@ -4,17 +4,15 @@ const RecruitmentSection: React.FC = () => (
     <section className="mb-10">
         <div className="max-w-5xl mx-auto bg-white rounded-lg shadow px-4 md:px-8 py-8">
             <h2 className="text-2xl font-bold text-stone-700 mb-4">Recruitment</h2>
-            <p>2027保研及博士名额已满</p>
             <div className="text-base text-gray-700 space-y-4">
                 <p>
-                    We are looking for highly self-motivated Master's/Ph.D. students/postdoctoral researchers/research assistants interested in memory-centric AI systems, computing-in-memory technologies, hardware/software co-design, and efficient AI hardware.
-                </p>
-                <p>
-                    Prospective PhD students, master students, postdoctoral researchers, research assistants, and visiting students are welcome to contact us with a CV, transcripts, and publications if any.
+                    We are looking for highly self-motivated Postdocs, Ph.D./Master's students and research assistants interested in memory-centric AI systems, computing-in-memory technologies, hardware/software co-design, and efficient AI hardware.
+                    Feel free to reach out with a CV, transcripts, and publications if any.
                 </p>
 
+                <p>We are also actively recruiting postdoctoral researchers, with an annual salary of RMB 300,000. Outstanding candidates may apply for Boya Postdoctoral Fellowship, which offers a total annual package of at least RMB 500,000.</p>
                 <p>
-                    Please email{" "}
+                    Email: {" "}
                     <a
                         href="mailto:scwang@pku.edu.cn"
                         className="text-stone-700 underline hover:text-stone-900"
